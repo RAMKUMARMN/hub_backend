dfcc85a1037827e84b82c20128bd2d04b553f35a
Ameen Anwar <ameenanwar100@gmail.com>
2026-06-22 13:26:02 +0530

    Added audit log system and fixe some previous issues

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,18 +1,19 @@
-from app.models.user import User
-from app.models.chat import ChatSession, ChatMessage
-from app.models.document import Document
-from app.models.todo import Todo
-from app.models.poll import PollResponse
-from app.models.notification_job import NotificationJob
-from .audit import AuditLog
+from app.routers.auth import router as auth_router
+from app.routers.chat import router as chat_router
+from app.routers.documents import router as documents_router
+from app.routers.todos import router as todos_router
+from app.routers.admin import router as admin_router
+from app.routers.poll import router as poll_router
+from app.routers.notification_jobs import router as notification_jobs_router
+from app.routers.audit import router as audit_router
 
 __all__ = [
-    "User",
-    "ChatSession",
-    "ChatMessage",
-    "Document",
-    "Todo",
-    "PollResponse",
-    "NotificationJob",
-    "AuditLog",
+    "auth_router",
+    "chat_router",
+    "documents_router",
+    "notification_jobs_router",
+    "todos_router",
+    "admin_router",
+    "poll_router",
+    "audit_router",
 ]
